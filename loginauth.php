2c72fb503abede5b95433b080e60039b5173c9e5
noczero <zeroonetm@gmail.com>
2017-09-08 17:50:17 +0700

    dua

--- loginauth.php
+++ loginauth.php
@@ -1,3 +1,4 @@
+<?php include 'config/db.php' ?>
 
 <!DOCTYPE html>
 <html>
@@ -11,18 +12,24 @@
 	$password = $_POST['password'];
 
 
-	echo <div>
-		$username
-	</div>;
+	$query = "SELECT id FROM user WHERE username='$username' AND password='$password'";
 
-	echo <div>
-		$password
-	</div>;
-	
+	if ($koneksi->query($query) === TRUE) {
+	    echo "New record created successfully";
+	} else {
+	    echo "ERROR";
+	}
+
+
+	if ($hasil->num_rows = 0) {
+		echo "<p> Username Password Salah</p>";
+	} else {
+		echo "<p> Username Password Benar</p>";
+	}
 
  ?>
 
-
+ <p> TEST </p>
 </body>
 
 </html>
